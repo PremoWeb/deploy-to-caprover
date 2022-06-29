1aa45f9eb1ee23a09b2bd223c226dcacf750929d
Nick Maietta <nick@premoweb.com>
2022-06-29 14:19:51 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,1 @@
-caprover deploy --appName $INPUT_APP --appToken $INPUT_TOKEN --branch $INPUT_BRANCH --caproverUrl $INPUT_SERVER --tarFile /github/workspace/temporary-captain-to-deploy.tar
+caprover deploy --appName $INPUT_APP --appToken $INPUT_TOKEN --caproverUrl $INPUT_SERVER --tarFile /github/workspace/temporary-captain-to-deploy.tar
